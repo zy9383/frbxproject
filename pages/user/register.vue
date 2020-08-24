b9a99d6f883364f1813cf8438d15d1d945e02c63
zy_9383 <zy_9383@163.com>
2020-08-24 16:33:04 +0800

    放入背景图

---
+++ pages/user/register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="register">
+    <!-- <img src="../../assets/registerbg.png" alt=""> -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.register {
+  width: 1920px;
+  height: 1080px;
+  margin-left: 0 !important;
+  background: url(../../assets/registerbg.png) no-repeat;
+}
+</style>
